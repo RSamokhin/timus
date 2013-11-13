bd31187656523a24bcab2d3088b19d3c5367c079
Roman Samokhin <poshliemail@googlemail.com>
2013-11-13 14:57:22 +0400

    http://acm.timus.ru/problem.aspx?space=1&num=1877

---
+++ 1877/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int code_a = in.nextInt();
+        int code_b = in.nextInt();
+        String result ="no";
+        if ((code_a%2==0)||(code_b%2==1)){
+            result = "yes";
+        }
+        out.println(result);
+        out.flush();
+    }
+}
