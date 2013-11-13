823ca5c964dd3950dfbb6adef354a37791b954c1
Roman Samokhin <poshliemail@googlemail.com>
2013-11-13 14:50:42 +0400

    http://acm.timus.ru/problem.aspx?space=1&num=1785

---
+++ 1785/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int table[] = new int[] {1,5,10,20,50,100,250,500,1000 };
+        String table_value[] =  new String[] {"few","several","pack","lots","horde","throng","swarm","zounds","legion"};
+        int number = in.nextInt();
+        String result = "";
+        int i = table.length-1;
+        do {
+           if (number >= table[i]){
+               result = table_value[i];
+           }else{
+               i--;
+           }
+        }while (result == "");
+        out.println(result);
+        out.flush();
+    }
+}
