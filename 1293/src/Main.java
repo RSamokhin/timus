db2780fb08fea7b76104c3c6bdf6127db12411c1
Roman Samokhin <poshliemail@googlemail.com>
2013-11-13 15:03:03 +0400

    http://acm.timus.ru/problem.aspx?space=1&num=1293

---
+++ 1293/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int result = 0;
+        int n = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        result = 2*n*a*b;
+        out.println(result);
+        out.flush();
+    }
+}
