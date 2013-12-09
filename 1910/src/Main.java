f701687c589a23220335211a36fcb7dfd168b684
Roman Samokhin <poshliemail@googlemail.com>
2013-12-09 16:27:24 +0400

    http://acm.timus.ru/problem.aspx?space=1&num=1910

---
+++ 1910/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int i = 0 ; i < a.length ; i++)
+            a[i] = in.nextInt();
+        int max = 0;
+        int max_id = 1;
+        for(int i = max_id; i < a.length-1 ; i++){
+            if (a[i-1]+a[i]+a[i+1]>max){
+                max = a[i-1]+a[i]+a[i+1];
+                max_id=i;
+            }
+        }
+        out.println(max);
+        out.println(max_id + 1);
+        out.flush();
+    }
+
+}
