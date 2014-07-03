9da5e0ef8994516929a7e4dbf48f49cf180e95b3
Roman <poshliemail@googlemail.com>
2014-07-03 16:22:27 +0400

    http://acm.timus.ru/problem.aspx?space=1&num=1924

---
+++ 2001/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int a1 = in.nextInt();
+        int b1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b2 = in.nextInt();
+        int a3 = in.nextInt();
+        int b3 = in.nextInt();
+        out.println(a1-a3);
+        out.println(b1-b2);
+        out.flush();
+    }
+}
