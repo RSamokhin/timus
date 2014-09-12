93fc19eb617bda14407d1d6d216712821a49e29d
Roman <poshliemail@googlemail.com>
2014-09-12 12:39:34 +0400

    http://acm.timus.ru/problem.aspx?space=1&num=1313

--- 1313/src/Main.java
+++ 1313/src/Main.java
@@ -7,17 +7,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int n = in.nextInt();
-        int nn[][] = new int[n][n];
-        int result[][] = new int[n*n][2];
-        for (int i = 0 ; i < n; i++){
-            for (int j = 0 ; j < n; j++){
-                nn[j][i] = in.nextInt();
-            }
-        }
-        for (i = 0 ; i < n ; i++){
-            result[n][1] = i 
-        }
-        
+
         
         
         //out.println(result);
