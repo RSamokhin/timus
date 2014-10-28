51e0e1545673f9bbed9b6dd9cdf2e6f91055f909
Roman <poshliemail@googlemail.com>
2014-10-28 17:39:57 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1225

--- 1225/src/Main.java
+++ 1225/src/Main.java
@@ -7,14 +7,20 @@ public class Main {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int n = in.nextInt();
-        int r = 0;
-        
-        
-        
-        
-        
-        
-        out.println(r);
+        long f0=0, f1=1;
+        long fn=f1;
+        if (n > 2){
+            for (int i = 2 ; i <n ; i++){
+                long t=f1;
+                fn = fn+f1;
+                f1 = f1 +f0;
+                f0=t;
+                 
+            }
+        }else{
+            fn = f1;
+        }
+        out.println(2*fn);
         out.flush();
     }
 } 
