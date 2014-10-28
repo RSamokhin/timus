d8347cf073f835495d39a65dcd0006b002f89702
Roman <poshliemail@googlemail.com>
2014-10-28 16:18:39 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1581

--- 1581/src/Main.java
+++ 1581/src/Main.java
@@ -6,8 +6,32 @@ public class Main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
-        out.println();
+        int n = in.nextInt();
+        int[] a = new int[n];
+        int c = 0;
+        for (int i = 0 ; i < n ; i++){
+                int y = in.nextInt();
+                a[i]= y;
+                if(c>0){
+                    if ((a[i-1]==y)){
+                        c++;
+                    }else{
+                        out.print(c);
+                        out.print(" ");
+                        out.print(a[i-1]);
+                        out.print(" ");
+                        c=1;
+                    }
+                }else{
+                   c++;
+                }
+                if (i==n-1){
+                    out.print(c);
+                    out.print(" ");
+                    out.print(a[i]);
+                    out.print(" ");
+                }
+        }
         out.flush();
     }
 }
