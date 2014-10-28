f41e3b74778c006bf55222a13488bbfd44526d49
Roman <poshliemail@googlemail.com>
2014-10-28 16:53:17 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1225

---
+++ 1225/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt();
+        int r = 0;
+        
+        
+        
+        
+        
+        
+        out.println(r);
+        out.flush();
+    }
+} 
