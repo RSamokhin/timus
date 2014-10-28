46d493730b2dc51038b80474227a42c51ec02cb7
Roman <poshliemail@googlemail.com>
2014-10-28 14:30:17 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1567

---
+++ 1567/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        char[] m = in.nextLine().toCharArray();
+        int n = 0;
+        String d = ".,! ";
+        for (int i = 0 ; i < m.length ; i ++){
+            char l = m[i];
+            int h;
+            if (d.indexOf(l)>0)
+                h = (d.indexOf(l))%3+1;
+            else
+                h = (l - 'a')%3 + 1;
+           n+=h;
+        }
+        out.println(n);
+        out.flush();
+    }
+}
