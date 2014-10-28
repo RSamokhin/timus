eb88edf00a310b890232d6b05dfb1c63d77505cc
Roman <poshliemail@googlemail.com>
2014-10-28 13:11:17 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1068

---
+++ 1068/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt();
+        int s = ((n<0)? (-1) : 1)*(n*(n+((n<2)? (-1) : 1))/2) + ((n<2) ? 1 : 0);
+        out.println(s);
+        out.flush();
+    }
+}
