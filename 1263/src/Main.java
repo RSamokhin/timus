1e5661d2b972e1bb79be40815c627fd7d2d601fc
Roman <poshliemail@googlemail.com>
2014-10-28 22:46:36 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1263

---
+++ 1263/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.text.*;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt(),m = in.nextInt();
+        double [] a = new double[n];
+        for (int i = 0 ; i < m ; i ++){
+            a[in.nextInt()-1]++;
+        }
+        for (int j = 0 ; j < n ; j ++){
+          DecimalFormat twoDForm = new DecimalFormat("#0.00");
+          out.println(twoDForm.format((a[j]*100)/m)+"%");
+        }
+        out.flush();
+    }
+
+} 
