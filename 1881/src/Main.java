dde85cc746c69e63dbd680cb5620a1f25c597bf9
Roman <poshliemail@googlemail.com>
2014-10-29 12:17:30 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1881

--- 1881/src/Main.java
+++ 1881/src/Main.java
@@ -1,16 +1,26 @@
 import java.io.*;
 import java.util.*;
-import java.math.*;
-import java.text.*;
-
-
 public class Main {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-       
+        int h = in.nextInt() , w = in.nextInt() , n = in.nextInt();
+        int l = 0 , c = -1 ; 
+        for (int i = 0 ; i < n ; i++){
+            String t  = in.next();
+            if (c + 1 + t.length() <= w)
+                c=c+1+t.length();
+            else{
+                l++;
+                c = t.length() ;
+            }    
+        }
+        if (c>-1) l++;
+        if(l%h>0)
+            out.println(l/h+1);
+        else
+            out.println(l/h);
         out.flush();
     }
-
 } 
