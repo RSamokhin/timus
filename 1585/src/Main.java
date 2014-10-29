5052689f93a6db5dc04c89fbce693efc7833fff9
Roman <poshliemail@googlemail.com>
2014-10-29 11:05:54 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1585

--- 1585/src/Main.java
+++ 1585/src/Main.java
@@ -9,7 +9,36 @@ public class Main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int n = in.nextInt();
+        int a=0,b=0,c=0;
+        for (int i = 0 ; i < n ; i++){
+            String t = in.next();
+            in.next();
+            switch(t){
+                case "Emperor":
+                    a++;
+                    break;
+                case "Macaroni":
+                    b++;
+                    break;
+                case "Little":
+                    c++;
+                    break;       
+            }
+        }
+        if (a>b){
+            if (a>c){
+                out.println("Emperor Penguin");
+            }else{
+                out.println("Little Penguin");
+            }
+        }else{
+            if (b>c){
+                out.println("Macaroni Penguin");
+            }else{
+                out.println("Little Penguin");
+            }
+        }
         out.flush();
     }
 
