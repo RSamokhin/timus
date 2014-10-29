80d36e9abf1f9f9968a12250448912f3855db4dd
Roman <poshliemail@googlemail.com>
2014-10-29 10:46:44 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1585

---
+++ 1585/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.text.*;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        
+        out.flush();
+    }
+
+} 
