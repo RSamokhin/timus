b86053546721579a8e8e89ccc9df18c05e92cc4e
RSamokhin <RSamokhin@RA.RVLTD.RU>
2014-10-30 22:55:24 +0400

    http://acm.timus.ru/problem.aspx?space=1&num=1100 buble sort

--- 1100/src/main.java
+++ 1100/src/main.java
@@ -5,7 +5,34 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int n = in.nextInt();
+        int[][] a = new int[n][2];
+        for (int i = 0 ; i < n ; i ++){
+            int id = in.nextInt(), m = in.nextInt();
+            a[i][0] = id ; a[i][1] = m;
+            int j = i;
+            while(j>0){
+           // for (int j = i ; j > 0 ; j-- ){
+                if (a[j][1] > a[j-1][1]){
+                    int k = a[j][0];
+                    int l = a[j][1];
+                    a[j][0]=a[j-1][0];
+                    a[j][1]=a[j-1][1];
+                    a[j-1][0] = k;
+                    a[j-1][1] = l;
+                }else{
+                    j=0;
+                }
+                j--;
+            }   
+        }
+        //sout.println("==========");
+        for (int t = 0 ; t < n ; t++){
+            out.print(a[t][0]);
+            out.print(" ");
+            out.print(a[t][1]);
+            out.println();
+        }
         out.flush();
     }
 } 
