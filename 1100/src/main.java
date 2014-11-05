83ce44188809ac879d4a8f763c1691531ee9191c
Roman <poshliemail@googlemail.com>
2014-11-05 17:31:36 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1100
    рекурсивная быстрая сортировка

--- 1100/src/main.java
+++ 1100/src/main.java
@@ -1,35 +1,73 @@
 import java.io.*;
 import java.util.*;
+
 public class main {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int n = in.nextInt();
-        LinkedList<Integer[]> a = new LinkedList<>();
-        for (int i = 0 ; i < n ; i++){
-            int id = in.nextInt(), m = in.nextInt();
-            int l = 0;
-            int j = i;
-           // out.println("j = "+ j + "  l = " + l);
-            while (j!=l){               
-                if (a.get((j-l)/2+l)[1]>=m){                   
-                    l=(j-l)/2+l+1;
-                }else{
-                    j = (j-l)/2+l;
-                }
+        int[] []  a = new int[n][3];
+        for (int h = 0 ; h < n ; h++){
+            a[h][0] = in.nextInt();
+            a[h][1] = in.nextInt();
+            a[h][2] = h;
+        }
+        qSort(a,0,n-1);
+        
+     //   out.println("========");
+    //   сортировка по порядку
+        
+        Map A = new TreeMap<>();
+        int s = 0;
+        while ( s < n ){
+            int M = a[s][1];
+            while((s<n)&&(M==a[s][1])){
+                String b =  a[s][0] + " " + a[s][1];
+                A.put(a[s][2], b);
+                s++;
             }
-            Integer [] b = new Integer[2];
-            b[0]=id;b[1]=m;
-            a.add(j,b);
+            for (Iterator it = A.entrySet().iterator(); it.hasNext();) {
+                Map.Entry e = (Map.Entry) it.next();
+                out.println(e.getValue());
+            }
+            //out.println("++++++++++");
+            A.clear();
         }
-       // out.println("======");
-        for (int t = 0 ; t < n ; t++){
-            out.print(a.get(t)[0]);
-            out.print(" ");
-            out.print(a.get(t)[1]);
-            out.println();
-        }
+        
         out.flush();
+    } 
+    private static final Random random = new Random();
+    public static void qSort(int[][] array, int l, int r) {
+        int i = l;
+        int j = r;
+        int x = array[l + random.nextInt(r - l + 1)][1];
+        while (i <= j) {
+            while (array[i][1] > x) {
+                i++;
+            }
+            while (array[j][1] < x) {
+                j--;
+            }
+            if (i <=j) {
+                int t1 = array[i][0];
+                int t2 = array[i][1];
+                int t3 = array[i][2];
+                array[i][0] = array[j][0];
+                array[i][1] = array[j][1];
+                array[i][2] = array[j][2];
+                array[j][0] = t1;
+                array[j][1] = t2;
+                array[j][2] = t3;
+                i++;
+                j--;
+            }
+        }
+        if (l<j){
+            qSort(array, l, j);
+        }
+        if(i<r){
+            qSort(array, i, r);
+        }
     }
 } 
