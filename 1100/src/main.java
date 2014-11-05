134a78e1c79586a458f755f30e13b501ae305f83
Roman <poshliemail@googlemail.com>
2014-11-05 18:32:10 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1100
    Array.sort overrite 14 test

--- 1100/src/main.java
+++ 1100/src/main.java
@@ -7,67 +7,21 @@ public class main {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int n = in.nextInt();
-        int[] []  a = new int[n][3];
-        for (int h = 0 ; h < n ; h++){
-            a[h][0] = in.nextInt();
-            a[h][1] = in.nextInt();
-            a[h][2] = h;
+        int[][]  q = new int[n][2];
+        for (int i = 0 ; i < n ; i++){
+            q[i][0] = in.nextInt();
+            q[i][1] = in.nextInt();
         }
-        qSort(a,0,n-1);
-        
-     //   out.println("========");
-    //   сортировка по порядку
-        
-        Map A = new TreeMap<>();
-        int s = 0;
-        while ( s < n ){
-            int M = a[s][1];
-            while((s<n)&&(M==a[s][1])){
-                String b =  a[s][0] + " " + a[s][1];
-                A.put(a[s][2], b);
-                s++;
-            }
-            for (Iterator it = A.entrySet().iterator(); it.hasNext();) {
-                Map.Entry e = (Map.Entry) it.next();
-                out.println(e.getValue());
-            }
-            //out.println("++++++++++");
-            A.clear();
+        Arrays.sort(q, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return Integer.compare(o2[1], o1[1]);
+                }
+        });        
+       // out.println("+++++++++");
+        for (int j = 0 ; j < n ; j++){
+            out.println(q[j][0]+" "+q[j][1]);
         }
-        
         out.flush();
-    } 
-    private static final Random random = new Random();
-    public static void qSort(int[][] array, int l, int r) {
-        int i = l;
-        int j = r;
-        int x = array[l + random.nextInt(r - l + 1)][1];
-        while (i <= j) {
-            while (array[i][1] > x) {
-                i++;
-            }
-            while (array[j][1] < x) {
-                j--;
-            }
-            if (i <=j) {
-                int t1 = array[i][0];
-                int t2 = array[i][1];
-                int t3 = array[i][2];
-                array[i][0] = array[j][0];
-                array[i][1] = array[j][1];
-                array[i][2] = array[j][2];
-                array[j][0] = t1;
-                array[j][1] = t2;
-                array[j][2] = t3;
-                i++;
-                j--;
-            }
-        }
-        if (l<j){
-            qSort(array, l, j);
-        }
-        if(i<r){
-            qSort(array, i, r);
-        }
     }
 } 
