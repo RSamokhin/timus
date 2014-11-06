cf586b29f442c42953e016e7e35b2c435172b234
Roman <poshliemail@googlemail.com>
2014-11-06 15:14:43 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1991

---
+++ 1991/src/main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt(), k = in.nextInt();
+        long q=0,w=0;
+        for (int i = 0 ; i < n ; i++){
+            int t = in.nextInt();
+            if (t>k)
+                q+=t-k;
+            else
+                w+=k-t;
+        }        
+        out.println(q);
+        out.println(w);
+        out.flush();
+    }
+} 
