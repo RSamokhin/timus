88a64523a32222c893bbbe020d5b566edb853911
Roman <poshliemail@googlemail.com>
2014-11-08 14:33:37 +0300

    http://acm.timus.ru/submit.aspx?space=1&num=1110
    StringBuilder

--- 1110/src/main.java
+++ 1110/src/main.java
@@ -10,6 +10,7 @@ public class main {
         int n = in.nextInt(), m = in.nextInt(); 
         BigInteger y = in.nextBigInteger();
         ArrayList<String> a = new ArrayList<>();
+        StringBuilder d = new StringBuilder("");
         for (int j = 0 ; j < m ; j++){
             BigInteger r = new BigInteger("1");
             for (int i = 1 ; i <= n ; i++){
@@ -20,11 +21,12 @@ public class main {
                 a.add(String.valueOf(j));
             }
         }
-        if (a.size()>0)
+        if (a.size()>0){
             a.stream().forEach((f) -> {
-                out.print(f+" ");
-        });
-        else
+                d.append(f).append(" ");
+            });
+            out.print(d);
+    }else
             out.println(-1);
         out.flush();
     }
