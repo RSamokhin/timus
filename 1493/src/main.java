cbe67772906c44c7226e8761502aaabd0e645b8e
Roman <poshliemail@googlemail.com>
2014-11-08 13:26:35 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1493

---
+++ 1493/src/main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        
+        
+        out.flush();
+    }
+} 
