374625f5f28e235408aef8b9d98cf3ff631c1dd4
Roman <poshliemail@googlemail.com>
2014-11-10 20:12:31 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1349
    Firm theory

--- 1349/src/main.java
+++ 1349/src/main.java
@@ -6,7 +6,34 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-    
+        int n = in.nextInt();
+        int r = -1; 
+        int a = 1,b = 2,c=3;
+        boolean ready = false;
+        if (n<3){
+            for (int i = 1 ; i < 99 ; i ++ ){
+                for (int j = i+1; j < 100 ; j++){
+                    for (int k = j ; k <101 ; k++){
+                        if (Math.pow ( i,n ) + Math.pow ( j,n ) == Math.pow ( k,n )){
+                            a=i;b=j;c=k;
+                            ready = true;
+                            break;
+                        }
+                    }
+                    if (ready){
+                            break;
+                    }
+                }
+                if (ready){
+                    break;
+                }
+            }
+        }
+        if (ready){
+            out.println(a+" "+b+" "+c);
+        }else{
+            out.println(-1);
+        }
         out.flush();
     }
 } 
