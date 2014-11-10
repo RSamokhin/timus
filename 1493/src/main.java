a12d27c49c3af1ff520f6d403171f4b0a78e6e43
Roman <poshliemail@googlemail.com>
2014-11-10 18:24:13 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1493

--- 1493/src/main.java
+++ 1493/src/main.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
 public class main {
@@ -7,9 +6,15 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
- 
-        
-        
+        int n = in.nextInt();
+        int a = n/1000, b=n%1000;
+        if ((b+1<1000)&&(compute(b+1)==compute(a))||(b-1>=0)&&(compute(b-1)==compute(a)))
+            out.println("Yes");
+        else
+            out.println("No");
         out.flush();
+    }
+    public static int compute(int b){
+        return b%10+(b%100)/10+(b%1000)/100;
     }
 } 
