b4184fabb60d166686ec9d5e60de3c179ae76a50
Roman <poshliemail@googlemail.com>
2014-11-12 00:57:38 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1925

--- 1925/src/main.java
+++ 1925/src/main.java
@@ -6,7 +6,15 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int I = 0, Z = 0;
+        for (int i = 0 ; i < n ; i ++){
+            I=I+in.nextInt();
+            Z=Z+in.nextInt();
+        }
+        int t = I+k-Z-2*(n+1);
+        out.println((t>=0)?(t):("Big Bang!"));
         out.flush();
     }
 } 
