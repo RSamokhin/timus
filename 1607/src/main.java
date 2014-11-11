c6de752b66bbf7c09dedc1152a450fefbae8a8cc
Roman <poshliemail@googlemail.com>
2014-11-11 23:29:54 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1607

--- 1607/src/main.java
+++ 1607/src/main.java
@@ -6,7 +6,26 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int a=in.nextInt(),b=in.nextInt(),c=in.nextInt(),d=in.nextInt(),r=a;
+        boolean f = true;
+        if (a>c)
+            f=false;
+        while(f){
+            if (a+b<c){
+                a=a+b;
+                r=a;
+                if(c-d>a){
+                    c=c-d;
+                    r=c;
+                }else{
+                    f=false;
+                }
+            }else{
+                r=c;
+                f=false;
+            }
+        }
+        out.println(r);
         out.flush();
     }
 } 
