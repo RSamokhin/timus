96213f78ca0a855b889aa54a2bf3882ef413c503
Roman <poshliemail@googlemail.com>
2014-11-12 16:46:38 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1196

--- 1196/src/main.java
+++ 1196/src/main.java
@@ -6,7 +6,22 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
+        int n = in.nextInt();
+        int r = 0;
+        Map a = new HashMap<>();
+        for (int i = 0 ; i < n ; i++){
+            long y = in.nextInt();
+            a.put(y, 1);
+        }
+        int m = in.nextInt();
+        for (int j = 0 ; j < m ; j ++){
+            long t = in.nextInt();
+            if (a.containsKey(t)){
+                r++;
+                a.remove(t);
+            }
+        }
+        out.println(r);
         out.flush();
     }
 }
