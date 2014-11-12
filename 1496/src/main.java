2285fc0280f835656d3f66b6cef33d1f212a549a
Roman <poshliemail@googlemail.com>
2014-11-12 12:36:01 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1496

--- 1496/src/main.java
+++ 1496/src/main.java
@@ -6,7 +6,20 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int n = in.nextInt();
+        Map<String, Integer> h = new HashMap<>();
+        Map<String, Integer> o = new HashMap<>();
+        for (int i = 0 ; i < n ; i++){
+            String t = in.next();
+            if (h.get(t)==null)
+                h.put(t,1);
+            else{
+                if (o.get(t)==null){
+                    out.println(t);
+                    o.put(t, 1);
+                }
+            }
+        }
         out.flush();
     }
 }
