464fd325705583dc0d6f08a21badab63468f33db
Roman <poshliemail@googlemail.com>
2014-11-12 13:12:14 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1636

--- 1636/src/main.java
+++ 1636/src/main.java
@@ -6,7 +6,12 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
+        int t1 = in.nextInt(), t2 = in.nextInt();
+        int t = 0;
+        for (int i = 0 ; i < 10 ; i ++){
+            t = t + 20*in.nextInt();
+        }
+        out.println((t1+t>t2)?"Dirty debug :(":"No chance.");  
         out.flush();
     }
 }
