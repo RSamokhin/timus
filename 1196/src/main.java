d1549abc14f626bc96665e1aab999744bc238faf
Roman <poshliemail@googlemail.com>
2014-11-12 18:21:09 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1196

--- 1196/src/main.java
+++ 1196/src/main.java
@@ -1,24 +1,23 @@
 import java.io.*;
 import java.util.*;
-import java.util.TreeSet;
 
 public class main {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        Scanner in = new Scanner(System.in);
+        BufferedInputStream bis = new BufferedInputStream(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        int n = in.nextInt();
+        int n = readInt(bis);
         int r = 0;
         TreeSet<Integer> a = new TreeSet<>();
         for (int i = 0 ; i < n ; i++){
-            int y = in.nextInt();
+            int y = readInt(bis);
             a.add(y);
         }
         int max = a.last();
         int min = a.first();
-        int m = in.nextInt();
+        int m = readInt(bis);
         for (int j = 0 ; j < m ; j ++){
-            int t = in.nextInt();
+            int t = readInt(bis);
             if ((t<=max)||(t>=min))
                 if (a.contains(t)){
                     r++;
@@ -26,5 +25,17 @@ public class main {
         }
         out.println(r);
         out.flush();
+    }
+    private static int readInt(InputStream in) throws IOException {
+            int ret = 0;
+            boolean dig = false;
+
+            for (int c = 0; (c = in.read()) != -1; ) {
+                if (c >= '0' && c <= '9') {
+                    dig = true;
+                    ret = ret * 10 + c - '0';
+                } else if (dig) break;
+        }
+        return ret;
     }
 }
