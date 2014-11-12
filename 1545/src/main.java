312e17123cecf740ec3536427419ced2b0113a5b
Roman <poshliemail@googlemail.com>
2014-11-12 12:04:42 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1545

--- 1545/src/main.java
+++ 1545/src/main.java
@@ -6,7 +6,18 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        String [] a = new String[26];
+        for (int i = 0 ; i < 26 ; i ++){
+            a[i]="";
+        }
+        int n = in.nextInt();
+        for (int j = 0 ; j < n ; j ++){
+            String t = in.next();
+            int p = "abcdefghijklmnopqrstuvwxyz".indexOf(t.substring(0, 1));
+            a[p]=a[p]+"\n"+t;
+        }
+        String f = in.next();
+        out.println(a["abcdefghijklmnopqrstuvwxyz".indexOf(f)]);
         out.flush();
     }
-} 
+}
