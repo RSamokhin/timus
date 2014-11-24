f1d0dc1dd4a7fe66e0e4487e266dc8c86e06637b
Roman <poshliemail@googlemail.com>
2014-11-25 00:40:27 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1837

--- 1837/src/main.java
+++ 1837/src/main.java
@@ -1,23 +1,72 @@
 import java.io.*;
+import java.util.*;
 
 public class main {
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) 
     {
-        BufferedInputStream bis = new BufferedInputStream(System.in);
+        Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
-        out.flush();
-    }
-    private static int readInt(InputStream in) throws IOException {
-            int ret = 0;
-            boolean dig = false;
+        int n = in.nextInt();
+        ArrayList<String> a = new ArrayList<>();
+        ArrayList<String> b = new ArrayList<>();
+        for (int i = 0 ; i < n ; i ++){
+            String q=in.next(), w = in.next(), e=in.next();
+            a.add(q);a.add(q);a.add(w);a.add(w);a.add(e);a.add(e);
+            b.add(w);b.add(e);b.add(q);b.add(e);b.add(q);b.add(w);
+        }        
+        ArrayList<String> r = new ArrayList<>();
+        ArrayList<Integer> v = new ArrayList<>();
+        int max = 0;
+        if (a.indexOf("Isenbaev")>=0){
+            r.add("Isenbaev");
+            v.add(0);
+            int checkIndex = 0;
+            while (checkIndex<r.size()){
+                String checkName = r.get(checkIndex);
+                while(a.indexOf(checkName)>=0){
+                    int indexA = a.indexOf(checkName);
+                    int indexR = r.indexOf(checkName);
+                    int currentWeight = v.get(indexR);
+                      //добавить условиеменьшего веса
+                    int iter = 2;
+                    while (iter>0){
+                        String inA = b.get(indexA);
+                        if (r.contains(inA)){
+                               int position = r.indexOf(inA);
+                               int inbaseWeight = v.get(position);
+                               v.set(position,(currentWeight+1>inbaseWeight)?inbaseWeight:currentWeight+1);
 
-            for (int c = 0; (c = in.read()) != -1; ) {
-                if (c >= '0' && c <= '9') {
-                    dig = true;
-                    ret = ret * 10 + c - '0';
-                } else if (dig) break;
+                        }else{
+                            r.add(b.get(indexA)); v.add(currentWeight+1);
+                        }
+                        a.remove(indexA);b.remove(indexA);
+                        iter--;
+                    }
+                }
+                checkIndex++;
+            }
         }
-        return ret;
+            /*
+            for(String s : a){
+                if (!r.contains(s)){
+                    r.add(s);
+                    v.add(-1);
+                }
+            }
+            */
+        a.stream().filter((s) -> (!r.contains(s))).map((s) -> {
+            r.add(s);
+                return s;
+        }).forEach((_item) -> {
+            v.add(-1);
+        });
+        TreeMap <String, Integer> resultMap = new TreeMap<>();
+        for(int l = 0 ; l < r.size(); l++){
+            resultMap.put(r.get(l),v.get(l));
+        }
+        resultMap.keySet().stream().forEach((key) -> {
+            out.println(key+" "+(resultMap.get(key)!=-1?resultMap.get(key):"undefined"));
+        });
+        out.flush();
     }
 }
