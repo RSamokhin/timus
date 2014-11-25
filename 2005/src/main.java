0cc8074de70f81804d38802f49689b29c3d6cbd6
Roman <poshliemail@googlemail.com>
2014-11-25 23:56:49 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=2005

---
+++ 2005/src/main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int [][] a = new int[5][5];
+        for (int i = 0 ; i < 5 ; i++)
+            for (int j = 0 ; j < 5 ; j++)
+                a[i][j]=in.nextInt();
+        TreeMap<Integer,String> m = new TreeMap<>();
+        m.put(a[0][1]+a[1][2]+a[2][3]+a[3][4], "1 2 3 4 5");
+        m.put(a[0][2]+a[2][1]+a[1][3]+a[3][4], "1 3 2 4 5");
+        m.put(a[0][2]+a[2][3]+a[3][1]+a[1][4], "1 3 4 2 5");
+        m.put(a[0][3]+a[3][2]+a[2][1]+a[1][4], "1 4 3 2 5");
+        out.println(m.firstKey());
+        out.println(m.get(m.firstKey()));
+        out.flush();
+    }
+}
