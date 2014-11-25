196f37c72e60d7c21a0a230c9530364bf6cddcbf
Roman <poshliemail@googlemail.com>
2014-11-26 00:14:06 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1617

--- 1617/src/main.java
+++ 1617/src/main.java
@@ -6,7 +6,20 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
+        int n = in.nextInt();
+        HashMap<Integer,Integer> h = new HashMap<>();
+        for (int i = 0 ; i < n ; i ++){
+            int check = in.nextInt();
+            if(h.containsKey(check))
+                h.put(check,h.get(check)+1);
+            else
+                h.put(check, 1);
+        }
+        int r = 0;
+        for (int k : h.keySet()) {
+            r=r+h.get(k)/4;
+        }
+        out.println(r);
         out.flush();
     }
 }
