0502e2a98aa7de4af67b0c116fdc81392d54214a
Roman <poshliemail@googlemail.com>
2014-11-26 00:15:01 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1723

---
+++ 1723/src/main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        
+        out.flush();
+    }
+}
