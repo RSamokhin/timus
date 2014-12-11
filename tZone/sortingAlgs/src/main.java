3a9bf90711c57bb43fadda6ffb5a1347d684b8b5
Roman <poshliemail@googlemail.com>
2014-12-11 11:33:07 +0300

    bubble sort added

---
+++ tZone/sortingAlgs/src/main.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class main {
+    public static void main(String[] args) 
+    {
+        PrintWriter out = new PrintWriter(System.out);
+        int n = 10;
+        int[] a = new int[n];
+        for(int i = 0 ; i < n ; i++ ){
+            a[i]=(int)(Math.random()*100);
+            out.print(a[i]+" ");
+        }
+        a = mergeSort(a,n);
+        //a = bubbleSort(a);
+        out.println();
+        for (int i:a)
+            out.print(i+" ");
+        out.flush();
+    }
+    ////bubblesort
+    private static int[] bubbleSort(int[] a){
+        for (int j=a.length-1 ; j>=0 ; j--){
+            for (int g = 0 ; g < j; g++){
+                if (a[j]<a[g]){
+                    int t = a[j];
+                    a[j]=a[g];
+                    a[g]=t;
+                }
+            }
+        }
+        return a;
+    }
+    ////mergesort
+    private static int[] mergeSort(int[] a,int n){
+        if (n == 1) 
+                return a;
+        int middle = n/2;
+        int [] left =  new int[middle];
+        int [] right = new int[n-middle];
+        for (int i = 0 ; i < n ; i++)
+            if(i<middle)
+                left[i]=a[i];
+            else
+                right[i-middle]=a[i];
+        return merge(mergeSort(left,middle),mergeSort(right,n-middle));         
+        
+    }
+    public static int[] merge(int[] a,int[] b){
+        if (a.length==0)
+            return b;
+        if (b.length==0)
+            return a;
+        if (a[0]<b[0]){
+            int t = a[0];
+            int[] r = new int[a.length-1];
+            for(int i = 0 ; i < r.length ; i++){
+                r[i]= a[i+1];
+            }
+            int[] k;
+            IntStream first = (IntStream) IntStream.builder().add(t);
+            IntStream second = Arrays.stream(merge(r,b));
+            k = IntStream.concat(first,second).toArray(int[]::new);
+           
+        }
+        else
+            return a;
+    }
+}
