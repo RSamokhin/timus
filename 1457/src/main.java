7d161700923570e8407a9f1e337ac409eb835f94
Roman <poshliemail@googlemail.com>
2014-12-17 01:29:55 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1457

--- 1457/src/main.java
+++ 1457/src/main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class main {
@@ -6,7 +7,11 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
+        int n = in.nextInt();
+        double s=0;
+        for (int i =0 ; i < n ; i++)
+            s+=in.nextInt();
+        out.println(String.format(String.format(new DecimalFormat("#0.000000").format(s/n))));
         out.flush();
     }
 }
