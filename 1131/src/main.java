54342dd60d52a8a6b33ea7d412191c0ef28dedeb
Roman <poshliemail@googlemail.com>
2014-12-16 17:18:32 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1131

--- 1131/src/main.java
+++ 1131/src/main.java
@@ -6,7 +6,16 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
+        int n = in.nextInt()-1, k = in.nextInt();
+        int i=1;
+        int s=0;
+        while ((i<k)&&(n>0)){
+            n=n-i;
+            i=((i*2<k)?(i*2):(k));
+            s++;
+        }
+        s=s+n/k+(((n%k)>0)?1:0);
+        out.print(s);
         out.flush();
     }
 }
