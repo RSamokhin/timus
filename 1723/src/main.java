25fc1127357359ce58c7240b30522fd4a4754f00
Roman <poshliemail@googlemail.com>
2014-12-16 13:13:53 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1723

--- 1723/src/main.java
+++ 1723/src/main.java
@@ -1,12 +1,30 @@
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class main {
     public static void main(String[] args) 
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        TreeMap <String,Integer> a = new TreeMap<>();
+        String input = in.next();
+        String[] s;
+        s = input.split("");
+        for(String t:s){
+            a.putIfAbsent(t, 0);
+            a.put(t, a.get(t)+1);
+        }
+        ArrayList<Entry<String,Integer>> c = new ArrayList<>();
+        c.addAll(a.entrySet());
+        c.sort(new EntryComparator());
+        out.println(c.get(0).getKey());
         out.flush();
+    }
+}
+class EntryComparator implements Comparator<Entry<String,Integer>>{
+    @Override
+    public int compare(Entry<String,Integer> a,Entry<String,Integer> b){
+        return a.getValue().compareTo(b.getValue())*(-1);
     }
 }
