16f65c361a18dcd2748793e1a10d0715d58ee274
Roman <poshliemail@googlemail.com>
2014-12-16 13:52:17 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1654

--- 1654/src/main.java
+++ 1654/src/main.java
@@ -6,7 +6,19 @@ public class main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        StringBuilder b = new StringBuilder();
+        char [] s = in.next().toCharArray();
+        for(char t:s){
+            if(b.length()>0){
+                if (t==b.charAt(b.length()-1)){
+                    b.deleteCharAt(b.length()-1);
+                }else{
+                    b.append(t);
+                }
+            }else
+                b.append(t);
+        }
+        out.print(b);
         out.flush();
     }
 }
