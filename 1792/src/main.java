8a94ddc717b4e9bba983b4b2d8c0ca4ffe523fbd
Roman <poshliemail@googlemail.com>
2014-12-17 18:53:29 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1792

---
+++ 1792/src/main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int [] a = new int[7];
+        for(int i = 0 ; i < 7 ; i++)
+            a[i]=in.nextByte();
+        if (!((a[4]==(a[1]+a[2]+a[3])%2)&&(a[5]==(a[0]+a[2]+a[3])%2)&&(a[6]==(a[0]+a[1]+a[3])%2))){
+            for(int p = 0 ; p < 7; p++){
+                int t = a[p];
+                a[p]= ((a[p]+1)%2);
+                if(!((a[4]==(a[1]+a[2]+a[3])%2)&&(a[5]==(a[0]+a[2]+a[3])%2)&&(a[6]==(a[0]+a[1]+a[3])%2)))
+                    a[p]=t;
+                else{
+                   // out.println(p);
+                    break;
+                }
+            }
+        }
+        for (int i : a)
+            out.print(i+" ");
+        out.flush();
+    }
+}
