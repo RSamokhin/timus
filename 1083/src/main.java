38663dd24e91f1f3e4082f037efae7111e2cd3cb
rsamokhin <poshliemail@googlemail.com>
2014-12-24 02:24:32 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1083

---
+++ 1083/src/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.PrintWriter;
+public class main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt();
+        int k = in.next().length();
+        int i = 1, s=n;
+        while (n-i*k>0){
+            s *= n - k * i;
+            i++;
+        }
+        out.println(s);
+        out.flush();
+    }
+}
