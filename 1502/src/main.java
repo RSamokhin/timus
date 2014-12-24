738069fe28d79c921714120d6304a21f1dcd1292
rsamokhin <poshliemail@googlemail.com>
2014-12-24 19:23:06 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1502

---
+++ 1502/src/main.java
@@ -0,0 +1,11 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class main{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        long n = in.nextInt();
+        out.println((n+2)*(((0+n)*(n+1))/2));
+        out.flush();
+    }
+}
