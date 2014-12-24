cddcaac150410bf0f40e4820e1357bddeca6a0f5
rsamokhin <poshliemail@googlemail.com>
2014-12-25 01:48:19 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1902

---
+++ 1902/src/main.java
@@ -0,0 +1,15 @@
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        double n = in.nextInt(), t = in.nextInt(), s = in.nextInt();
+        DecimalFormat dc = new DecimalFormat(".000000");
+        for (int i = 0 ; i < n ; i ++){
+            out.println(dc.format((s+(in.nextInt()-s+t)/2)));
+        }
+        out.flush();
+    }
+}
