26830a90ea70a3bdee4b03bd9b6d86e3fa255c25
rsamokhin <poshliemail@googlemail.com>
2014-12-25 18:55:00 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1139

---
+++ 1139/src/main.java
@@ -0,0 +1,19 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int n = in.nextInt(),m = in.nextInt();
+        int nod = 1;
+        int a=(n-1),b=(m-1);
+        while((a!=0)&&(b!=0)){
+            if (a>b)
+                a=a%b;
+            else
+                b=b%a;
+        }
+        out.println(m+n-a-b-2);
+        out.flush();
+    }
+}
