f6c0b5a7f5c76f385d59e0ce9c558d447130663b
rsamokhin <poshliemail@googlemail.com>
2014-12-26 01:05:20 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1290 AC sorted List

--- 1290/src/main.java
+++ 1290/src/main.java
@@ -1,36 +1,19 @@
 import java.io.PrintWriter;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Deque;
 import java.util.Scanner;
-import java.util.HashMap;
 import java.util.List;
 public class main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int n = in.nextInt();
-        HashMap<Integer,Integer> h = new HashMap<>();
-        for (int i = 0 ; i < n ; i++){
-            int t = in.nextInt();
-            for (int j = 0 ; j < t ; j++){
-                h.putIfAbsent(j, 0);
-                h.put(j, h.get(j)+1);
-            }
-        }
-        Deque<Integer> a = new ArrayDeque<>();
-        List list = new ArrayList();
-        list.addAll(h.values());
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0 ; i < n ; i++)
+            list.add(in.nextInt());
         Collections.sort(list);
-        a.addAll(list);
-        while (!a.isEmpty()){
-            int t=a.peekFirst();
-            out.println(a.size());
-            while((!a.isEmpty())&&(a.peekFirst()==(t))){
-                a.removeFirst();
-            }
-        }
+        for (int k = list.size()-1;k>=0;k--)
+            out.println(list.get(k));
         out.flush();
     }
 }
