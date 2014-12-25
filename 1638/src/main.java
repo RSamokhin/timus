08a516b72bdd641b9d4cb070834b23a290b4bb37
rsamokhin <poshliemail@googlemail.com>
2014-12-25 19:16:56 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1638

--- 1638/src/main.java
+++ 1638/src/main.java
@@ -4,7 +4,8 @@ public class main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int f = in.nextInt(), t = in.nextInt(), n = in.nextInt(), o = in.nextInt();
+        out.println((Math.abs(o-n)+((n<o)?-1:1))*f+Math.abs(o-n)*2*t);
         out.flush();
     }
 }
