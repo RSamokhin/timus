a6d47b540c9235280e6f7a454fb2cf8a58b5a6f1
rsamokhin <poshliemail@googlemail.com>
2014-12-30 18:15:19 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1106

--- 1106/src/main.java
+++ 1106/src/main.java
@@ -1,6 +1,7 @@
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.HashSet;
+import java.util.TreeSet;
 public class main{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
@@ -9,29 +10,48 @@ public class main{
         boolean isFalse = false;
         HashSet<Integer>a = new HashSet<>();
         HashSet<Integer>b = new HashSet<>();
+        in.nextLine();
         for (int i = 1 ; i <= n ; i++){
-            int t = in.nextInt();
-            if (t!=0){
-                boolean noa = a.contains(i);
-                boolean nob = b.contains(i);
-                if ((!noa&&!nob)){
-                    a.add(i);
-                    noa = true;
-                }
-                    while(t!=0){
-                        if(t==0)
-                            break;
-                        if (t>i)
-                            if (noa)
-                                b.add(t);
-                            else
-                                a.add(t);
-                        t = in.nextInt();
-                    }
-            }else{
-               isFalse=true;
+           String[] m = in.nextLine().split(" ");
+           if (isFalse){
+               continue;
+           }
+           if(m.length == 1){
+               isFalse = true;
+           }
+           int [] t = new int[m.length - 1];
+           for (int v = 0 ; v < m.length-1 ; v++)
+               t[v]=Integer.parseInt(m[v]);
+           boolean IinA = a.contains(i);
+           boolean IinB = b.contains(i);
+           if(!IinA&&!IinB){
+               for (int g : t){
+                       if(a.contains(g)){
+                           b.add(i);
+                           IinB=true;
+                           break;
+                       }
+                       if(b.contains(g)){
+                           a.add(i);
+                           IinA=true;
+                           break;
+                       }
+                       a.add(i);
+                       IinA=true;
+               }
+           }
+           if (IinA){
+               for (int g : t){
+                         if (!a.contains(g))
+                            b.add(g);
+               }
+           }
+           if (IinB){
+                for (int g : t){
+                        if (!b.contains(g))
+                            a.add(g);
+               }
             }
-            
         }
         if (isFalse)
             out.println(0);
