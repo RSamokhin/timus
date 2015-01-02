73a908548a4664c33654450ff869bf262bf8cb51
rsamokhin <poshliemail@googlemail.com>
2015-01-03 00:46:39 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1180

--- 1180/src/main.java
+++ 1180/src/main.java
@@ -4,7 +4,12 @@ public class main{
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        String s = in.next();
+        char[] t = s.toCharArray();
+        int n = 0;
+        for (char f : t)
+           n+=f;
+        out.println((n%3==0)?(2):("1"+"\n"+n%3));
         out.flush();
     }
 }
