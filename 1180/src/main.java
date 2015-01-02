d4e29939f4aa1704d8336bae0de71950e12e7c29
rsamokhin <poshliemail@googlemail.com>
2015-01-02 19:03:40 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1180

---
+++ 1180/src/main.java
@@ -0,0 +1,10 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class main{
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        
+        out.flush();
+    }
+}
