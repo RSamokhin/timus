9bac94a0d6f7a97c797fc071a28087e5969414a1
rsamokhin <poshliemail@googlemail.com>
2015-01-02 19:00:55 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=2035

--- 2035/src/main.java
+++ 2035/src/main.java
@@ -4,7 +4,15 @@ public class main{
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        long x = in.nextLong(), y = in.nextLong(), c = in.nextLong();
+        if (c <= Math.max(x, y))
+            out.println( (x==Math.max(x, y))?(c+" "+0):(0+" "+c)  );
+        else
+            if (x+y<c)
+                out.println("Impossible");
+            else
+                out.println(  (x==Math.min(x, y))?(x+" "+(c-x)):((c-y)+" "+y)  );
+                //out.println(Math.min(x, y)+" "+(c-Math.min(x, y)));
         out.flush();
     }
-}
+} 
