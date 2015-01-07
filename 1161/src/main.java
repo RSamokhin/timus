d7cfb2f73ac5d47b16ec1a6fefc728a36bf41a10
rsamokhin <poshliemail@googlemail.com>
2015-01-08 00:11:02 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1161 correct but Runtime error (non-zero exit code)

--- 1161/src/main.java
+++ 1161/src/main.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.TreeSet;
+import java.text.DecimalFormat;
 public class main{
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int n = in.nextInt();
+        TreeSet<Double>a = new TreeSet<>();
+        for (int i = 0 ; i < n ; i++)
+            a.add(in.nextDouble());
+        while(a.size()>1){
+            a.add(2*Math.sqrt(a.pollLast()*a.pollLast()));
+        }
+        DecimalFormat b = new DecimalFormat(".00");
+        out.println(b.format(a.pollFirst()));
         out.flush();
     }
 }
