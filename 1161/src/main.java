45128926d3ecfad4ac01f63e2f254c09083f2710
rsamokhin <poshliemail@googlemail.com>
2015-01-08 00:31:15 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1161 correct but Runtime error (non-zero exit code) - ACM TIMUS JAVA issue

--- 1161/src/main.java
+++ 1161/src/main.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 import java.io.PrintWriter;
-import java.util.TreeSet;
+import java.util.List;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 public class main{
-    public static void main(String[]args){
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int n = in.nextInt();
-        TreeSet<Double>a = new TreeSet<>();
+        List<Double>a = new ArrayList<>();
         for (int i = 0 ; i < n ; i++)
             a.add(in.nextDouble());
+        Collections.sort(a);
+        double s = a.get(a.size()-1)/4;
         while(a.size()>1){
-            a.add(2*Math.sqrt(a.pollLast()*a.pollLast()));
+            s = 2*Math.sqrt(s*a.get(a.size()-1));
+            a.remove(a.size()-1);
         }
         DecimalFormat b = new DecimalFormat(".00");
-        out.println(b.format(a.pollFirst()));
+        out.println(b.format(s));
         out.flush();
     }
 }
