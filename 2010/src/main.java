cf70aa751e0a00092eadbbfaf90357f86c7cf807
rsamokhin <poshliemail@googlemail.com>
2015-01-10 22:24:50 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=2010 AC

--- 2010/src/main.java
+++ 2010/src/main.java
@@ -1,44 +1,34 @@
 import java.util.Scanner;
 import java.io.PrintWriter;
-import java.util.HashMap;
 public class main{
     public static void main(String[]args){
+        int n = 0;
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        long n = in.nextLong();
-        long x = in.nextLong();
-        long y = in.nextLong();
-        long King;
-        if (n==1)
-            King = 0;
-        else
-            if ((((x==1)||(x==n))&&((y==1)||(y==n)))||(n==2))
-                King = 3;
-            else
-                if (((x==1)||(x==n))||((y==1)||(y==n)))
-                    King=5;
-                else
-                    King = 8;
-        long Knight;
-        if ((n<3)||((n==3)&&(x==2)&&(y==2)))
-            Knight = 0;
-        else
-            if ((((x==1)||(x==n))&&((y==1)||(y==n)))||(n==3))
-                Knight = 2;
-            else
-                if ((((x==1)||(x==n))&&((y==2)||(y==n-1)))||(((x==2)||(x==n-1))&&((y==1)||(y==n))))
-                    Knight = 3;
-                else
-                    if (((x==2)||(x==n-1))&&((y==2)||(y==n-1)))
-                        Knight = 4;
-                    else
-                        if (((x==2)||(x==n-1))||((y==2)||(y==n-1)))
-                           Knight = 6;
-                        else
-                            Knight = 8;
-        long Bishop = Math.min(n-x, n-y)+Math.min(n-x, y-1)+Math.min(n-y, x-1)+Math.min(x-1, y-1);
-        long Rook = 2*n-2;
-        long Queen = Bishop+Rook;
+        n = in.nextInt();
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int King = 0;
+            King +=(checkIn(x-1,y-1,n))?1:0;
+            King +=(checkIn(x-1,y,n))?1:0;
+            King +=(checkIn(x-1,y+1,n))?1:0;
+            King +=(checkIn(x,y-1,n))?1:0;
+            King +=(checkIn(x,y+1,n))?1:0;
+            King +=(checkIn(x+1,y-1,n))?1:0;
+            King +=(checkIn(x+1,y,n))?1:0;
+            King +=(checkIn(x+1,y+1,n))?1:0;
+        int Knight = 0;
+            Knight +=(checkIn(x-1,y-2,n))?1:0;
+            Knight +=(checkIn(x-2,y-1,n))?1:0;
+            Knight +=(checkIn(x-2,y+1,n))?1:0;
+            Knight +=(checkIn(x-1,y+2,n))?1:0;
+            Knight +=(checkIn(x+1,y+2,n))?1:0;
+            Knight +=(checkIn(x+2,y+1,n))?1:0;
+            Knight +=(checkIn(x+2,y-1,n))?1:0;
+            Knight +=(checkIn(x+1,y-2,n))?1:0;
+        int Bishop = Math.min(n-x, n-y)+Math.min(n-x, y-1)+Math.min(n-y, x-1)+Math.min(x-1, y-1);
+        int Rook = 2*n-2;
+        int Queen = Bishop+Rook;
         out.println("King: "+King);
         out.println("Knight: "+Knight);
         out.println("Bishop: "+Bishop);
@@ -46,5 +36,8 @@ public class main{
         out.println("Queen: "+Queen);
         
         out.flush();
+    }
+    private static boolean checkIn(int x , int y, int n){
+        return ((x>0)&&(x<=n)&&(y>0)&&(y<=n));
     }
 }
