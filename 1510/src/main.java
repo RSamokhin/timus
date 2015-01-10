917bc1e347c7bf98a51a1466999b8a34246f5fd0
rsamokhin <poshliemail@googlemail.com>
2015-01-10 04:03:44 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1510 MLE 21 with list sort n/2

--- 1510/src/main.java
+++ 1510/src/main.java
@@ -2,23 +2,21 @@ import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 public class main{
     public static void main(String[]args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));        
         PrintWriter out = new PrintWriter(System.out);
         int n = Integer.parseInt(in.readLine());
-        HashMap <Integer,Integer> h = new HashMap<>(n/2);
+        List<Integer> h = new ArrayList<>();
         for (int i = 0 ; i < n ; i++){
             int t = Integer.parseInt(in.readLine());
-            h.putIfAbsent(t, 0);
-            int c = h.get(t);
-            if (c+1>n/2){
-                out.println(t);
-                break;
-            }
-            h.put(t, c+1);
+            h.add(t);
         }
+        Collections.sort(h);
+        out.println(h.get(n/2));
         out.flush();
     }
 }
