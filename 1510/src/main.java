edcce091ae9b7eabea603cdb294cfd20d1f9c32d
rsamokhin <poshliemail@googlemail.com>
2015-01-10 04:10:34 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1510 AC with array and Arrays.sort

--- 1510/src/main.java
+++ 1510/src/main.java
@@ -2,21 +2,18 @@ import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 public class main{
     public static void main(String[]args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));        
         PrintWriter out = new PrintWriter(System.out);
         int n = Integer.parseInt(in.readLine());
-        List<Integer> h = new ArrayList<>(n);
+        int[]a = new int[n];
         for (int i = 0 ; i < n ; i++){
-            int t = Integer.parseInt(in.readLine());
-            h.add(t);
+            a[i] = Integer.parseInt(in.readLine());
         }
-        Collections.sort(h);
-        out.println(h.get(n/2));
+        Arrays.sort(a);
+        out.println(a[n/2]);
         out.flush();
     }
 }
