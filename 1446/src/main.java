08f7def3c36670365573235f838163996471d299
rsamokhin <poshliemail@googlemail.com>
2015-01-11 03:12:11 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1446 AC with StringBuilder

---
+++ 1446/src/main.java
@@ -0,0 +1,39 @@
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashMap;
+public class main{
+    public static void main(String [] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int n = Integer.parseInt(in.readLine());
+        StringBuilder s = new StringBuilder("Slytherin:\n");
+        StringBuilder h = new StringBuilder("Hufflepuff:\n");
+        StringBuilder g = new StringBuilder("Gryffindor:\n");
+        StringBuilder r = new StringBuilder("Ravenclaw:\n");
+        for (int i = 0 ; i < n ; i++){
+            String name = in.readLine();
+            String faculty = in.readLine();
+            switch(faculty){
+                case("Slytherin"):
+                    s.append(name).append("\n");
+                     break;
+                case("Hufflepuff"):
+                    h.append(name).append("\n");
+                     break;
+                case("Gryffindor"):
+                    g.append(name).append("\n");
+                     break;
+                case("Ravenclaw"):
+                    r.append(name).append("\n");
+                     break;
+            }
+        }
+        out.println(s);
+        out.println(h);
+        out.println(g);
+        out.println(r);
+        out.flush();
+    }
+}
