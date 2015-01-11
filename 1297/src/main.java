5bae242d204a4c277942b959de6e997fb50fedea
rsamokhin <poshliemail@googlemail.com>
2015-01-12 02:27:26 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1297 AC n^2 with matrix

---
+++ 1297/src/main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class main{
+    public static void main(String[]args)throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        String s = in.readLine();
+        char[] c = s.toCharArray();
+        int b = 0;
+        int l  = 1;
+        for (int i = 0 ; i < c.length ; i++){
+            int j = 0 ;
+            while((i-j>=0)&&((i+j)<c.length)){
+                if (c[i-j]==c[i+j]){
+                    if(j*2+1>l){
+                        b=i-j;
+                        l=j*2+1;
+                    }
+                }else
+                    break;
+                j++;
+            }
+            j=0;
+            while((i-j>=0)&&((i+j+1)<c.length)){
+                if(c[i-j]==c[i+j+1]){
+                    if((j+1)*2>l){
+                        b=i-j;
+                        l=(j+1)*2;
+                    }
+                }else
+                    break;
+                j++;
+            }
+        }
+        out.println(s.substring(b, b+l));
+        out.flush();
+    }
+}
