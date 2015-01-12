4490e9bd74a02b88072c7ad8c4ce3bc311535b17
rsamokhin <poshliemail@googlemail.com>
2015-01-12 03:11:30 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1149 AC

--- 1149/src/main.java
+++ 1149/src/main.java
@@ -1,12 +1,48 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) 
-    {
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
 
-        out.flush();
+    public static void a(int n) {
+        for(int i=1;i<=n;i++){
+            if(i==n){
+                System.out.print("sin("+Integer.toString(i));
+            }else{
+                if(i%2==1){
+                    System.out.print("sin("+Integer.toString(i)+"-");
+                }else{
+                    System.out.print("sin("+Integer.toString(i)+"+");
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(")");
+        }
+    }
+
+    public static void s(int n){
+        for(int i=0;i<n-1;i++){
+            System.out.print("(");
+        }
+        for(int i=1;i<n;i++){
+            a(i);
+            System.out.print("+"+Integer.toString(n+1-i)+")");
+        }
+        a(n);
+        System.out.print("+1");
+    }
+
+    public static void main (String[] args) {
+        try {
+
+            Scanner in = new Scanner(System.in);
+
+            int n=in.nextInt();
+
+            s(n);
+
+        } catch (Exception ex) {
+
+            System.out.println(ex.toString());
+        }
     }
 }
