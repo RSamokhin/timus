68671c26a7188db28b0b136570bde5c3e648e6a0
rsamokhin <poshliemail@googlemail.com>
2015-01-12 17:28:03 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1876 AC

--- 1876/src/Main.java
+++ 1876/src/Main.java
@@ -5,11 +5,12 @@ public class Main {
     {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        int a = in.nextInt(), b = in.nextInt(),s=0, o = 0;
-        while(o<80){
-            
-        }
-        out.print(s);
+        int a = in.nextInt(), b = in.nextInt(),s=0;
+        int o;
+        a -= 39; b-=39;
+        o = 39+39*2;
+        o+=Math.max((1+(a-1)*2+1),(b*2)+1);
+        out.println(o);
         out.flush();
     }
 } 
