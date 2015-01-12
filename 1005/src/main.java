5d892ab78524cb44fd119017d89710906dff10c9
rsamokhin <poshliemail@googlemail.com>
2015-01-12 14:21:06 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1005 WA5 differencing algorithm

--- 1005/src/main.java
+++ 1005/src/main.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.PriorityQueue;
+import java.util.Collections;
 public class main{
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
+        int n = in.nextInt();
+        PriorityQueue<Integer> h = new PriorityQueue<>(n,Collections.reverseOrder());
+        for (int i = 0 ; i < n ; i++){
+            h.add(in.nextInt());
+        }
+        while(h.size()>1){
+            int a = h.poll();
+            int b = h.poll();
+            h.add(a-b);
+        }
+        out.println(h.poll());
         out.flush();
     }
 }
