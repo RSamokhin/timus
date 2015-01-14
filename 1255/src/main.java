b2009b98be7eb2277ffe7170881acef54d72d2a9
rsamokhin <poshliemail@googlemail.com>
2015-01-15 02:12:34 +0300

    http://acm.timus.ru/problem.aspx?space=1&num=1255 wrong way 5-3 decision \n 1 2 3 3 3

---
+++ 1255/src/main.java
@@ -0,0 +1,12 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+public class main{
+    public static void main(String[]args){
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(), l = in.nextInt();
+        int s = (n*(n/l))+((n%l)*(n/l));
+        out.println(s);
+        out.flush();
+    }
+}
