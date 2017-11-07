539b1051a653a7f71b9698581e9eb6c11d4ffcfa
RSamokhin <poshliemail@googlemail.com>
2017-11-07 22:29:09 +0300

    1330

---
+++ src/t1330.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+data = []
+sums = [0]
+for i in range(n):
+    sums.append(sums[i] + int(sys.stdin.readline()))
+
+q = int(sys.stdin.readline())
+for i in range(q):
+    # inputs = map(lambda x: int(x), sys.stdin.readline().split(' '))
+    print sums[inputs[1]] - sums[inputs[0] - 1]
