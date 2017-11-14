4dcc0ac3081c7afc3031468538da79f1de8f11b2
RSamokhin <poshliemail@googlemail.com>
2017-11-14 15:46:07 +0300

    set solution

--- leetcode/0003.longest-substring-without-repeating-characters.js
+++ leetcode/0003.longest-substring-without-repeating-characters.js
@@ -2,21 +2,40 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLongestSubstring = function(s) {
+//     let sub = 0;
+//     let currentSub = '';
+//     for (let i = 0; i < s.length; i++) {
+//         let indexInCurrentSub = currentSub.indexOf(s[i]);
+//         if (indexInCurrentSub === -1) {
+//             currentSub += s[i];
+//             sub = Math.max(currentSub.length,sub);
+//         } else {
+//             sub = Math.max(currentSub.length,sub);
+//             currentSub = currentSub.substr(indexInCurrentSub + 1);
+//             currentSub += s[i];
+//         }
+//     }
+//     return sub;
+// };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var lengthOfLongestSubstring = function(s) {
-    let sub = 0;
-    let currentSub = '';
-    for (let i = 0; i < s.length; i++) {
-        let indexInCurrentSub = currentSub.indexOf(s[i]);
-        if (indexInCurrentSub === -1) {
-            currentSub += s[i];
-            sub = Math.max(currentSub.length,sub);
+    let l = 0; // frame length
+    let i = 0; // frame start point
+    while (l + i < s.length) {
+        let substr = s.substring(i, i + l + 1);
+        let set = new Set(substr);
+        if (set.size === substr.length) {
+            l++; // enlarge frame size
         } else {
-            sub = Math.max(currentSub.length,sub);
-            currentSub = currentSub.substr(indexInCurrentSub + 1);
-            currentSub += s[i];
+            i++; // move frame
         }
     }
-    return sub;
+    return l;
 };
 
-console.log(lengthOfLongestSubstring('abca'));
+console.log(lengthOfLongestSubstring("a"));
