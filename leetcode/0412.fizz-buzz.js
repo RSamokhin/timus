4cbe359843c2897218fba8998fa5ce7c63fbbfeb
RSamokhin <poshliemail@googlemail.com>
2017-11-18 23:31:02 +0300

    412 fizz buzz

---
+++ leetcode/0412.fizz-buzz.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const FIZZ = 'Fizz';
+const BUZZ = 'Buzz';
+var fizzBuzz = function(n) {
+    let fizz = 1, buzz = 1;
+    let result = [];
+    for (let i = 1; i <= n; i++) {
+        let np = i.toString();
+        console.log(fizz);
+        if (fizz === 3 && buzz === 5) {
+            np = `${FIZZ}${BUZZ}`;
+            fizz = 1;
+            buzz = 1;
+        } else if (buzz === 5) {
+            np = BUZZ;
+            buzz = 1;
+            fizz ++;
+        } else if (fizz === 3) {
+            np = FIZZ;
+            fizz = 1;
+            buzz ++;
+        } else {
+            fizz ++;
+            buzz ++;
+        }
+        result.push(np);
+    }
+    return result;
+};
