14418162bc56a2924d82f18b7fc29e66f4ab5f53
RSamokhin <poshliemail@googlemail.com>
2017-11-22 23:08:28 +0300

    sqrtx binary search

--- leetcode/0069.sqrtx.js
+++ leetcode/0069.sqrtx.js
@@ -10,3 +10,21 @@ var mySqrt = function(x) {
     return i;
 };
 
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let l = 0;
+    let r = Math.pow(2,31);
+    while (true) {
+        let mid = l + Math.floor(r - l) / 2;
+        if (mid > x/mid) {
+            r = mid - 1;
+        } else {
+            if (mid + 1 > x / (mid + 1))
+                return mid;
+            l= mid + 1;
+        }
+    }
+};
