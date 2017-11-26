0a650210b4457a2937e967deeff92cc3ec6b2415
RSamokhin <poshliemail@googlemail.com>
2017-11-26 03:58:18 +0300

    88. Merge Sorted Array

---
+++ leetcode/0088.merge-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m, nums1.length - m);
+    nums2.splice(n, nums2.length - n);
+    let i = m + n - 1;
+    console.log(nums1, nums2, i)
+    while (i >= 0) {
+        if (nums1[m - 1] > nums2[n - 1] || nums2[n - 1] === undefined) {
+            nums1[i] = nums1[m - 1];
+            m--;
+        } else {
+            nums1[i] = nums2[n - 1];
+            n --;
+        }
+        i --;
+    }
+};
