fdf9197c89347e98e1d3469d661d19f42aae1d47
RSamokhin <poshliemail@googlemail.com>
2017-11-26 04:13:30 +0300

    Remove Duplicates from Sorted List

---
+++ leetcode/0083.remove-duplicates-from-sorted-list.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let current = head;
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+};
--- leetcode/0088.merge-sorted-array.js
+++ leetcode/0088.merge-sorted-array.js
@@ -9,7 +9,6 @@ var merge = function(nums1, m, nums2, n) {
     nums1.splice(m, nums1.length - m);
     nums2.splice(n, nums2.length - n);
     let i = m + n - 1;
-    console.log(nums1, nums2, i)
     while (i >= 0) {
         if (nums1[m - 1] > nums2[n - 1] || nums2[n - 1] === undefined) {
             nums1[i] = nums1[m - 1];
