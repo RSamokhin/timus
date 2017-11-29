e2af4240975f6fd5eb558cc1ba0fbd927ed0d1ed
RSamokhin <poshliemail@googlemail.com>
2017-11-29 16:38:16 +0300

    Linked List Cycle

---
+++ leetcode/0141.linked-list-cycle.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let hasCycle = false;
+    let slow = head, fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            hasCycle = true;
+            break;
+        }
+    }
+    return hasCycle;
+};
