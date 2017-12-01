c8999fe3ac9569ad8e8f1291d036d37307698eb1
RSamokhin <poshliemail@googlemail.com>
2017-12-02 00:05:35 +0300

    461. Hamming Distance

---
+++ leetcode/0461.hamming-distance.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+    let s1 = (x >>> 0).toString(2).split('').reverse();
+    let s2 = (y >>> 0).toString(2).split('').reverse();
+    let result = 0;
+    let i = 0;
+    while (s1[i] !== undefined || s2[i] !== undefined) {
+        result += (s1[i] | 0) !== (s2[i] | 0) ? 1 : 0;
+        i++;
+    }
+    return result;
+};
