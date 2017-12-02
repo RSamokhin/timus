d08407385f9dacd3f475fbecbee4e8b331e91c48
RSamokhin <poshliemail@googlemail.com>
2017-12-03 02:45:09 +0300

    boer-moor algorythm

--- leetcode/0169.majority-element.js
+++ leetcode/0169.majority-element.js
@@ -6,3 +6,25 @@ var majorityElement = function(nums) {
     nums.sort((a, b) => a - b);
     return nums[Math.floor(nums.length / 2)]
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count = 0;
+    let most;
+    nums.forEach(n => {
+        if (!count) {
+            most = n;
+            count ++;
+        } else {
+            if (n === most) {
+                count ++;
+            } else {
+                count --;
+            }
+        }
+    })
+    return most;
+};
