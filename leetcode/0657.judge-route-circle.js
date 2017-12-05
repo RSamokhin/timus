d6627b3ffb781ae75af873de4dccc15f2e6d9ccd
RSamokhin <poshliemail@googlemail.com>
2017-12-05 04:54:02 +0300

    657. Judge Route Circle

--- leetcode/0657.judge-route-circle.js
+++ leetcode/0657.judge-route-circle.js
@@ -7,3 +7,19 @@ var judgeCircle = function(moves) {
     [].forEach.call(moves, m => {steps[m] ++});
     return steps.L === steps.R && steps.U === steps.D;
 };
+
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let v = 0;
+    let g = 0;
+    [].forEach.call(moves, m => {
+        if (m === 'L') g ++;
+        else if (m === 'R') g --;
+        else if (m === 'U') v ++;
+        else v --;
+    });
+    return v === 0 && g === 0;
+};
