ecc9d45794f46e7b4b77347b072e42fc1375a412
RSamokhin <poshliemail@googlemail.com>
2017-12-09 19:44:25 +0300

    557. Reverse Words in a String III

---
+++ leetcode/0557.reverse-words-in-a-string-iii.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    function split(s) {
+        let result = [];
+        let w = '';
+        [].forEach.call(s, (l, i) => {
+            if (l === ' ' && w.length) {
+                result.push(w);
+                w = '';
+            }
+            else if (l !== ' ') {
+                w += l;
+            }
+            if (w.length && i === s.length - 1) {
+                result.push(w);
+            }
+        });
+        return result;
+    }
+
+    function join(arr, sep) {
+        let result = '';
+        arr.forEach((w, i) => {
+            result += w;
+            if (i != arr.length - 1) {
+                result += sep;
+            }
+        });
+        return result;
+    }
+
+    function reverse(w) {
+        return [].reduceRight.call(w, (a, b) => a + b, '');
+    }
+
+    return join(split(s).map(w => reverse(w)), ' ');
+};
