961db1c0b5d4e7d1b4ae497cabbdf91c6235cd12
RSamokhin <poshliemail@googlemail.com>
2017-12-11 23:57:34 +0300

    627. Swap Salary

--- leetcode/0500.keyboard-row.js
+++ leetcode/0500.keyboard-row.js
@@ -8,6 +8,6 @@ var findWords = function(words) {
         q: 1, w: 1, e: 1, r: 1, t: 1, y: 1, u: 1, i: 1, o: 1, p: 1,
         a: 2, s: 2, d: 2, f: 2, g: 2, h: 2, j: 2, k: 2, l: 2,
         z: 3, x: 3, c: 3, v: 3, b: 3, n: 3, m: 3
-    }
+    };
     return words.filter(w => [].every.call(w.toLowerCase(), (l, i, arr) => kb[l] === kb[arr[0]]));
 };
